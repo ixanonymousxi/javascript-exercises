d5d4595d4c4ad56e3377089999007abe05597fa8
Sarah <louise562@aol.com>
2022-04-30 12:18:18 -0500

    Completed javascript exercise 5.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+    if (typeof num1 !== "number" || num1 < 0 || typeof num2 !== "number" || num2 < 0){
+        return "ERROR";
+    }
 
+    let sum = num1 < num2 ? num1 : num2;
+    const largeNum = num1 < num2 ? num2 : num1;
+    for (let i = sum + 1; i <= largeNum; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
