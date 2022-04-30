44bb927ba632c043087d5afc7b814951c33da764
Sarah <louise562@aol.com>
2022-04-30 12:38:17 -0500

    Completed javascript exercise 7.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,10 @@
-const ftoc = function() {
+const ftoc = function (Fahrenheit) {
+  return Number(((Fahrenheit - 32) * (5/9)).toFixed(1));
 
 };
 
-const ctof = function() {
-
+const ctof = function(Clesius) {
+  return Number(((Clesius * (9/5)) + 32).toFixed(1));
 };
 
 // Do not edit below this line
