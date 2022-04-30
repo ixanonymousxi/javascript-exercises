ef58622a749f8d045cccf8579752c32eca882dee
Sarah <louise562@aol.com>
2022-04-30 12:19:26 -0500

    Incorrectly added and commited exercise 4. Correcting that with this commit. Exercise 4 completed.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+    let newArr = array;
 
+    for (let i = 0; i < args.length; i++) {
+        newArr = newArr.filter(arrayItem => arrayItem !== args[i]);
+    }
+
+    return newArr;
 };
 
 // Do not edit below this line
