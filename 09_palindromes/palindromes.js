61ea7ac7e714f178de16ddf0fbc998d4bf8a8f56
Sarah <louise562@aol.com>
2022-06-12 17:31:01 -0500

    Completed Palindrome exercise.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const newString = string.toLowerCase();
+    let arr = newString.split("");
 
+    let filteredArr = arr.filter((letter) => {
+        if(letter.match(/[a-z]/i)){
+            return true;
+        };
+    });
+
+    let origDirection = filteredArr.join('');
+    let reverseDirection = filteredArr.reverse().join('');
+
+    return (origDirection === reverseDirection);
 };
 
 // Do not edit below this line
