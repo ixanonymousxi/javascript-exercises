ffe2e7837a9fdf9631c71649d5b8c36a1b0f4914
Sarah <louise562@aol.com>
2022-06-19 21:40:24 -0500

    Completed Javascript exercise 12.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleList) {
+    let oldest = peopleList[0];
+    const thisYear = new Date().getFullYear();
 
+    peopleList.forEach((person) => {
+        const curOldest = oldest;
+        const curOldestDeath = curOldest.hasOwnProperty('yearOfDeath') ? curOldest['yearOfDeath'] : thisYear;
+        const curOldestBirth = curOldest['yearOfBirth'];
+        const curOldestAge = curOldestDeath - curOldestBirth;
+
+        const personDeath = person.hasOwnProperty('yearOfDeath') ? person['yearOfDeath'] : thisYear;
+        const personBirth = person['yearOfBirth'];
+        const personAge = personDeath - personBirth;
+
+        oldest = curOldestAge > personAge ? oldest : person;
+
+    });
+
+    return oldest;
 };
 
 // Do not edit below this line
