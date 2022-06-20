d0704e7e6599a81fdd6c4374da7455d9f2642af5
Sarah <louise562@aol.com>
2022-06-19 20:39:40 -0500

    Completed Javascript exercies 10.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    const intNum = parseInt(num);
 
+    if (intNum < 1){return "OOPS"}
+
+    let seq = [1,1];
+
+    while (seq.length < intNum){
+        let secLastNum = seq[seq.length - 2];
+        let lastNum = seq[seq.length - 1];
+
+        seq.push(secLastNum + lastNum);
+    }
+
+    return seq[intNum-1];
 };
 
 // Do not edit below this line
