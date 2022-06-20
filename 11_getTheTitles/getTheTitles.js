9119db5829f943a228bd5a236275b94cdfb82762
Sarah <louise562@aol.com>
2022-06-19 20:47:20 -0500

    Completed Javascript exercise 11.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(bookList) {
+    const titleArr = [];
 
+    bookList.forEach(book => {
+        titleArr.push(book['title']);
+    });
+
+    return titleArr;
 };
 
 // Do not edit below this line
